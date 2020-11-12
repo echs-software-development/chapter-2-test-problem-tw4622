8b2599bb9b51bb594507f4d3c1b445402d17bbd7
tw4622 <73349451+tw4622@users.noreply.github.com>
2020-11-12 15:18:54 +0000

    i finished

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,18 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
-  
+  Scanner scan = new Scanner(System.in);
+  System.out.println("what number do you want");
+  int num1 = scan.nextInt();
+  System.out.println("what other number do you want");
+  int num2 = scan.nextInt();
+
+  double num15 = (double) num1;
+  double num16 = (double) num2;
+
+  double x = (num15 + num16)/ 2;
+
+  System.out.println("The averge of the numbers is" + x);
   }
 }
